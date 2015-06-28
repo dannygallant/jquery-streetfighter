5d2b4ac32836fca91cc66f0f677db5829b22a580
dannygallant <danny.gallant@att.net>
2015-06-28 16:55:51 -0500

    app.js - mouse click events for throwing
    Events created to change to ready stance and show hadouken.

--- js/app.js
+++ js/app.js
@@ -3,10 +3,23 @@ $(document).ready(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
-
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		// hide ryu-ready and display ryu-throwing
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// play hadouken sound
+		// show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		// hide hadouken & stop sound
+		// hide ryu-throwing and show ryu-ready
 	});
 });
 
