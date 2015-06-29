d04da538fadaa10659781593ce968dd32244943c
dannygallant <danny.gallant@att.net>
2015-06-29 15:18:21 -0500

    renamed main.html to index.html

--- js/app.js
+++ js/app.js
@@ -83,20 +83,3 @@ function runIntro() {
     })
   })
 }
-
-
-
-// function runIntro() {
-//   $('.sf-logo').fadeIn(2500, function() {
-//     $(this).fadeOut(1000, function() {
-//       $('.tech-logos').fadeIn(2500, function() {
-//         $(this).fadeOut(1500, function() {
-      	
-//           $('.throw').fadeIn(1000);
-//           $('.look-cool').fadeIn(1000);
-
-//           });
-//         })
-//       })
-//     })
-//   }
