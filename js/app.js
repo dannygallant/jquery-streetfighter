8fcdbeb334bff85222e52254125b51f05655b84a
dannygallant <danny.gallant@att.net>
2015-06-29 10:26:35 -0500

    Ryu cool pose
    Added code to toggle cool pose when pressing the ‘x’ key.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.ryu').mouseenter(function(){
+		$('.ryu').mouseenter(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
@@ -17,16 +17,50 @@ $(document).ready(function(){
 			function() {
 				$(this).hide();
 				$(this).css('left', '520px');
-			}
-			);
+			});
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 		// hide hadouken & stop sound
 		// hide ryu-throwing and show ryu-ready
-	});
+	})
+
 });
+
+
+$(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-ready').hide();
+      $('.ryu-still').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(e) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    // }
+  });
+
+
+
+
+
+
+
+ // $(document).keydown(function(e) {
+ //        if (e.which == 88) {
+ //        	// alert('Press the x!');
+ //           $('.ryu-ready').hide();
+ //           $('.ryu-still').show();
+ //        }
+    // }).keyup(function(e) {
+    //     if (e.which == 88) {
+    //     	// alert('Press the x!');
+    //        // $('.ryu-ready').show();
+    //        // $('.ryu-cool').hide()};
+// });
+
+
 
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
